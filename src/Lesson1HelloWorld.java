af1400f5481179e5721520a4f73c3b255bf372d2
Andrew Long <andrew_long@mail.ru>
2018-09-10 20:48:55 +0300

    First commit to repository

---
+++ src/Lesson1HelloWorld.java
@@ -0,0 +1,7 @@
+//first program hello world
+
+public class Lesson1HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Lesson3Strings.java
@@ -0,0 +1,6 @@
+public class Lesson3Strings {
+        public static void main(String [] args){
+            int x=5;
+            String s = "Hello";
+        }
+    }
