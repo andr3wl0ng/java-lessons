d44c3423b100d3fbb2595d4be21921e57523c2e4
andriy.dovgiy@paazl.com <andriy.dovgiy@paazl.com>
2019-09-23 17:30:39 +0200

    passed one more lesson doWhile

---
+++ src/Lesson8DoWhile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lesson8DoWhile {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please, input 5");
+        int value = scanner.nextInt();
+        while (value!=5){
+            System.out.println("PLEASE, input 5");
+            value= scanner.nextInt();
+        }
+        System.out.println("Good job! Your input is " +value);
+    }
+}
