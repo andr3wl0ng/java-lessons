3775129a14d50614ffc6a472eb44ec8f5ed97ae9
andriy.dovgiy@paazl.com <andriy.dovgiy@paazl.com>
2019-09-24 12:17:20 +0200

    lesson 9

--- src/main/java/Lesson9BreakContinue.java
+++ src/main/java/Lesson9BreakContinue.java
@@ -3,5 +3,25 @@ package main.java;
 public class Lesson9BreakContinue {
     public static void main (String [] args){
 
+       int i = 0;
+       int ii=0;
+       while(true){
+           if(i == 15){
+               break;
+           }
+           System.out.println(i);
+           i++;
+       }
+        System.out.println("Out of cycle because i="+ i);
+        System.out.println("---------------------------");
+        System.out.println("Start to check 'continue'");
+
+       for (ii=0; ii<=15; ii++){
+           if(ii%2==0){
+               continue;
+           }
+           System.out.println("odd numbers "+ii);
+       }
+
     }
 }
