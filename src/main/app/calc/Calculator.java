326d27060b5679bdcba3c2ea0d075125bb00653e
andr3wl0ng <andriy.dovgiy@gmail.com>
2019-09-30 00:51:18 +0300

    calculator

---
+++ src/main/app/calc/Calculator.java
@@ -0,0 +1,67 @@
+package main.app.calc;
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        double number1 = getNumber();
+        char operation = getOperation();
+        double number2 = getNumber();
+        double result = getResult(number1, number2, operation);
+        System.out.println("result of operation is: "+result);
+    }
+
+
+    public static double getNumber() {
+        System.out.println("input your number: ");
+        double num;
+        if (scanner.hasNextDouble()) {
+            num = scanner.nextDouble();
+        } else {
+            System.out.println("Invalid input, Please input number");
+            scanner.next();//рекурсия
+            num = getNumber();
+        }
+        return num;
+    }
+
+
+    public static char getOperation(){
+        System.out.println("input operation: ");
+        char operation;
+        String str = scanner.nextLine();
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Invalid input, Please input number");
+            scanner.next();//рекурсия
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+
+    public static double getResult(double number1, double number2, char operation){
+        double result;
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = getResult(number1, number2, getOperation());
+        }
+        return result;
+    }
+}
