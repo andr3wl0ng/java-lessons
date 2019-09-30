9c3b0174f15b72db6ce68db35fa3586408b22abb
andriy.dovgiy@paazl.com <andriy.dovgiy@paazl.com>
2019-09-30 10:56:27 +0200

    lesson 10 and 14

---
+++ src/main/java/Lesson10Switch.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Lesson10Switch {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input your age");
+        int age = scanner.nextInt();
+        switch (age){
+            case 0:
+                System.out.println("you born");
+                break;
+            case 7:
+                System.out.println("you go to school");
+                break;
+            case 18:
+                System.out.println("you finish school");
+                break;
+            default:
+                System.out.println("no match");
+        }
+
+    }
+}
