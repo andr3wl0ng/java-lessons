f193d6d66ff22ca8d84e615e72905d8e82174233
andr3wl0ng <andriy.dovgiy@gmail.com>
2019-10-06 20:07:08 +0300

    array from 0 to 9 random with counting EVEN numbers

--- src/main/java/application/hw5/ArrayRandom0To9.java
+++ src/main/java/application/hw5/ArrayRandom0To9.java
@@ -1,12 +1,26 @@
 package application.hw5;
-
+/*
+Создайте массив из 15 случайных целых чисел из отрезка [0;9]
+Выведите массив на экран.
+Подсчитайте сколько в массиве чётных элементов и
+выведете это количество на экран на отдельной строке.
+*/
 public class ArrayRandom0To9 {
     public static void main(String[] args) {
         int[] array = new int[15];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 9);
-            System.out.print(array[i]+" ");
+            System.out.print(array[i] + " ");
         }
-
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length - 1; j++) {
+//                if (array[j] > array[j + 1]) {
+//                    int temp = array[j];
+//                    array[j] = array[j + 1];
+//                    array[j + 1] = temp;
+//                }
+//            }
+//
+//        }
     }
 }
